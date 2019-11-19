b95bb91c71b5a686e3a656150d27f52a61b3f350
Adem Çınar <cinar.adem@hotmail.com>
2019-11-19 17:33:29 +0300

    fixed

--- elasticsearch-vmss/install-elasticsearch.sh
+++ elasticsearch-vmss/install-elasticsearch.sh
@@ -126,7 +126,7 @@ configure_es()
 	echo "node.name: ${HOSTNAME}" >> /etc/elasticsearch/elasticsearch.yml
 	echo "discovery.zen.minimum_master_nodes: 2" >> /etc/elasticsearch/elasticsearch.yml
 	echo 'discovery.zen.ping.unicast.hosts: ["10.0.0.10", "10.0.0.11", "10.0.0.12"]' >> /etc/elasticsearch/elasticsearch.yml
-	echo "network.host: _site_" >> /etc/elasticsearch/elasticsearch.yml
+	echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
 	echo "bootstrap.memory_lock: true" >> /etc/elasticsearch/elasticsearch.yml
         echo "xpack.security.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
 
